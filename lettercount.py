cab0d48b8fa6a93ddbdc2ceb8687b14a5377e084
Neil Smith <neil.git@njae.me.uk>
2014-03-14 11:54:05 +0000

    Updated lettercount.py

--- lettercount.py
+++ lettercount.py
@@ -1,21 +1,15 @@
+from language_models import sanitise
 import collections
-import string
-
-def sanitise(text):
-    return [l.lower() for l in text if l in string.ascii_letters]
 
 corpora = ['shakespeare.txt', 'sherlock-holmes.txt', 'war-and-peace.txt']
-counts = collections.defaultdict(int)
+counts = collections.Counter()
 
 for corpus in corpora:
-    text = sanitise(open(corpus, 'r').read())
-    for letter in text:
-        counts[letter] += 1
+    text = sanitise(open(corpus).read())
+    counts.update(text)
 
 sorted_letters = sorted(counts, key=counts.get, reverse=True)
 
 with open('count_1l.txt', 'w') as f:
     for l in sorted_letters:
-        f.write("{0}\t{1}\n".format(l, counts[l]))
-        
-    
+        f.write("{}\t{}\n".format(l, counts[l]))
