38bf0effd8ed55869fa5abdef8ee0529e0ecd4c7
Neil Smith <neil.git@njae.me.uk>
2016-11-08 11:09:24 +0000

    Added formatting to the logger

--- cipherbreak.py
+++ cipherbreak.py
@@ -13,11 +13,22 @@ from multiprocessing import Pool
 
 import matplotlib.pyplot as plt
 
-logging.basicConfig(filename="cipher.log", level=logging.INFO)
-logger = logging.getLogger(__name__)
-# logger.setLevel(logging.WARNING)
+# logging.basicConfig(filename="cipher.log", level=logging.INFO)
+# logger = logging.getLogger(__name__)
+
+logger = logging.getLogger('cipherbreak')
+logger.setLevel(logging.WARNING)
 # logger.setLevel(logging.INFO)
 # logger.setLevel(logging.DEBUG)
+
+# create the logging file handler
+fh = logging.FileHandler("cipher.log")
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+
+# add handler to logger object
+logger.addHandler(fh)
+
 
 from cipher import *
 from language_models import *
